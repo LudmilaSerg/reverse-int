7631483fdec7846b271f75821b506f74aa6d267c
LudmilaSerg <mikoelectrik@gmail.com>
2021-03-07 20:42:53 +0300

    feat: done revere-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+
+module.exports = function reverse(n) {
+    let res;
+    if (n < 0) {
+        n = n * (-1);
+        res = n.toString().split('').reverse().join('')
+    } else {
+        res = n.toString().split('').reverse().join('');
+    }
+    return res;
 }
+
+
