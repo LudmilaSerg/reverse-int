9782cfcc6718bf4deb9e0bde91ffef7ff5fc3256
LudmilaSerg <mikoelectrik@gmail.com>
2023-08-10 15:41:59 +0300

    feat: task reverse ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 module.exports = function reverse (n) {
 
-    const number = n < 0 ? -1 : 1;
 
+    let NumToStr = Math.abs(n).toString().split('').reverse().join('');
 
-    let NumToStr = Math.abs(n).toString(n).split('').reverse().join('');
-
-
-    return  number * parseInt(NumToStr, 10);
+    return parseInt(NumToStr, 10) ;
 
 
 
